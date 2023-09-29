9e490a5afe1e5a83d55c49c583291e25becf8b9e
chaitanya sikakolu <52370283+sikakoluchaitanya@users.noreply.github.com>
2023-09-29 22:33:25 +0530

    Time: 103 ms (98.71%), Space: 14.3 MB (32.33%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.c
+++ 0932-monotonic-array/0932-monotonic-array.c
@@ -2,12 +2,18 @@ bool isMonotonic(int* nums, int numsSize) {
     bool inc = true;
     bool dec = true;
 
-    for (int i = 0; i < numsSize - 1; i++) {
+    for (int i = 0; i < numsSize - 1; i++) 
+    {
         if (nums[i] > nums[i + 1]) {
             inc = false;
         }
         if (nums[i] < nums[i + 1]) {
             dec = false;
+        }
+
+        if (!inc && !dec) 
+        {
+            return false;
         }
     }
 
